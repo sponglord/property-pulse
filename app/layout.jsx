773bc14cf6fe08ab0daf1b22b640e51071f398ff
nicholas <nicholas.spong@adyen.com>
2025-01-09 14:43:10 +0100

    Added styles and meta data

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,15 @@
-// The layout file represents the main entry point to our application as far as our folder & file struture goes
+// 1. The layout file represents the main entry point to our application as far as our folder & file struture goes
+// It is a React component that is *rendered on the server*
+
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+  title: "PropertyPulse | Find the perfect rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+};
+
+// 2. The pages we create, and how they show in the layout, comes through the children property
 const MainLayout = ({ children }) => {
   return (
     <html lang="en">
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,8 @@
+// This is a React component that is *rendered on the server*
+
+// The reason this shows up as the home page is 'cos this file is directly in the app folder and we have called the file 'page'
+// This is file based routing in action
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <div>My HomePage</div>;
 };
 export default HomePage;
