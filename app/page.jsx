b661e68b34c2fe878c7302683e850c6b2f9aca30
nicholas <nicholas.spong@adyen.com>
2025-01-10 15:22:09 +0100

    Added sub routes and navigation

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,19 @@
 
 // The reason this shows up as the home page is 'cos this file is directly in the app folder and we have called the file 'page'
 // This is file based routing in action
+
+// This page contains an example of doing navigation with a Link component c.f. using React's Router
+// (You can see Router in action in the file: [id] > page.jsx)
+
+import Link from "next/link";
+
 const HomePage = () => {
-  return <div>My HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">Welcome</h1>
+      {/* Avoid doing a full reload when the user clicks the link by using a Link comp rather than the <a> tag */}
+      <Link href="/properties">Show Properties</Link>
+    </div>
+  );
 };
 export default HomePage;
