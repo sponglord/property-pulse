50dff3e3d590077f66bc3635d9fae4f73cb1b962
nicholas <nicholas.spong@adyen.com>
2025-01-21 10:19:27 +0100

    Added prettier config file

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,21 @@
-import Hero from "@/components/Hero";
-import InfoBoxes from "@/components/InfoBoxes";
-import HomeProperties from "@/components/HomeProperties";
+import Hero from '@/components/Hero';
+import InfoBoxes from '@/components/InfoBoxes';
+import HomeProperties from '@/components/HomeProperties';
 // import connectDB from "@/config/database";
 
 const HomePage = async () => {
-  /**
-   * You can connect to a DB from any server rendered component - however in our particular application
-   * we’re creating RESTful endpoints - so API routes that will run functions to connect to the DB
-   */
-  // await connectDB(); // test db connection
+    /**
+     * You can connect to a DB from any server rendered component - however in our particular application
+     * we’re creating RESTful endpoints - so API routes that will run functions to connect to the DB
+     */
+    // await connectDB(); // test db connection
 
-  return (
-    <div>
-      <Hero />
-      <InfoBoxes />
-      <HomeProperties />
-    </div>
-  );
+    return (
+        <div>
+            <Hero />
+            <InfoBoxes />
+            <HomeProperties />
+        </div>
+    );
 };
 export default HomePage;
