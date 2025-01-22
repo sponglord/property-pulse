a63bec689835228d7c1e54be4be419c0b3c2bb77
nicholas <nicholas.spong@adyen.com>
2025-01-22 12:11:09 +0100

    HomeProperties page also retrieves data from db

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,39 +1,48 @@
-import properties from "@/properties.json";
-import PropertyCard from "@/components/PropertyCard";
-import Link from "next/link";
+import PropertyCard from '@/components/PropertyCard';
+import Link from 'next/link';
 
-const HomeProperties = () => {
-  const recentProperties = properties
-    .sort(() => Math.random() - Math.random()) // N.B. trick to randomise an array
-    .slice(0, 3);
+const HomeProperties = async () => {
+    async function fetchProperties() {
+        try {
+            // Because we are doing this from the server we have to include the domain
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
 
-  return (
-    <>
-      <section className="px-4 py-6">
-        <div className="container-xl lg:container m-auto">
-          <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
-            Recent Properties
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {recentProperties === 0 ? (
-              <p>No Properties Found</p>
-            ) : (
-              recentProperties.map((property) => (
-                <PropertyCard key={property._id} property={property} />
-              ))
-            )}
-          </div>
-        </div>
-      </section>
-      <section className="m-auto max-w-lg my-10 px-6">
-        <Link
-          href="/properties"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Properties
-        </Link>
-      </section>
-    </>
-  );
+            if (!res.ok) {
+                throw new Error('Failed to fetch data');
+            }
+
+            return res.json();
+        } catch (error) {
+            console.log('### Data fetch error:: error=', error);
+        }
+    }
+
+    const properties = await fetchProperties();
+
+    const recentProperties = properties
+        .sort(() => Math.random() - Math.random()) // N.B. trick to randomise an array
+        .slice(0, 3);
+
+    return (
+        <>
+            <section className="px-4 py-6">
+                <div className="container-xl lg:container m-auto">
+                    <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">Recent Properties</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {recentProperties === 0 ? (
+                            <p>No Properties Found</p>
+                        ) : (
+                            recentProperties.map((property) => <PropertyCard key={property._id} property={property} />)
+                        )}
+                    </div>
+                </div>
+            </section>
+            <section className="m-auto max-w-lg my-10 px-6">
+                <Link href="/properties" className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">
+                    View All Properties
+                </Link>
+            </section>
+        </>
+    );
 };
 export default HomeProperties;
