804492b2d859494174fdf1307207ac50da69abfa
nicholas <nicholas.spong@adyen.com>
2025-01-22 12:14:18 +0100

    Update comment

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -3,6 +3,7 @@ import Property from '@/models/Property';
 
 // GET api/properties
 export const GET = async (request) => {
+    // For testing the route...
     // Going to http://localhost:5040/api/properties will print "Hello world" to the screen
     /* try {
         return new Response(JSON.stringify({ message: 'Hello world' }), { status: 200 });
@@ -12,6 +13,9 @@ export const GET = async (request) => {
     } */
 
     try {
+        // Now, going to http://localhost:5040/api/properties OR, within a comp, doing a fetch to this url
+        // will retrieve the properties data from the db
+
         await connectDB();
 
         // The Property model has a find method which takes options on how you want to retieve your data...
