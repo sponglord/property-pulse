bf5619bc54fec57274bbbfe719192e8a8dc7e450
nicholas <nicholas.spong@adyen.com>
2025-01-22 12:07:00 +0100

    Properties page retieves data from db

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,21 +1,41 @@
-import properties from "@/properties.json";
-import PropertyCard from "@/components/PropertyCard";
+import PropertyCard from '@/components/PropertyCard';
 
-const PropertiesPage = () => {
-  return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <p>No properties found</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
-  );
+// This is a server component - so we are "allowed" to make this db call
+async function fetchProperties() {
+    try {
+        // Because we are doing this from the server we have to include the domain
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        return res.json();
+    } catch (error) {
+        console.log('### Data fetch error:: error=', error);
+    }
+}
+
+const PropertiesPage = async () => {
+    const properties = await fetchProperties();
+
+    // Sort properties by date
+    properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+    return (
+        <section className="px-4 py-6">
+            <div className="container-xl lg:container m-auto px-4 py-6">
+                {properties.length === 0 ? (
+                    <p>No properties found</p>
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {properties.map((property) => (
+                            <PropertyCard key={property._id} property={property} />
+                        ))}
+                    </div>
+                )}
+            </div>
+        </section>
+    );
 };
 export default PropertiesPage;
