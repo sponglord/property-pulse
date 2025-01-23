eef49438bbd29e4a5c51800fae6e59a099984c68
nicholas <nicholas.spong@adyen.com>
2025-01-23 15:22:09 +0100

    Added SessionProvider wrapper

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,25 @@
-import Navbar from "@/components/Navbar";
-import "@/assets/styles/globals.css";
-import Footer from "@/components/Footer";
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
+import '@/assets/styles/globals.css';
 
 export const metadata = {
-  title: "PropertyPulse | Find the perfect rental",
-  description: "Find your dream rental property",
-  keywords: "rental, find rentals, find properties",
+	title: 'PropertyPulse | Find the perfect rental',
+	description: 'Find your dream rental property',
+	keywords: 'rental, find rentals, find properties',
 };
 
 const MainLayout = ({ children }) => {
-  return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
-  );
+	return (
+		<AuthProvider>
+			<html lang='en'>
+				<body>
+					<Navbar />
+					<main>{children}</main>
+					<Footer />
+				</body>
+			</html>
+		</AuthProvider>
+	);
 };
 export default MainLayout;
