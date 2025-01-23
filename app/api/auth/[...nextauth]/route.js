fa971f1f4b74f0e8466a49245a93551191741589
nicholas <nicholas.spong@adyen.com>
2025-01-23 15:44:49 +0100

    Added comment

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,13 @@
 import { authOptions } from '@/utils/authOptions';
 import NextAuth from 'next-auth';
 
+/**
+ * Anything in the URL after /auth will get picked up by Next-Auth
+ * Going to http://localhost:5040/api/auth/signin will show a "Sign in with Google button"
+ * And, if your Google credentials are correct in the .env file, clicking on that button will show you a screen
+ * where you can choose an account to login to the PropertyPulse App we created at https://console.cloud.google.com
+ */
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
