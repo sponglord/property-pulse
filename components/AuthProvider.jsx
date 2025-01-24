f834f9cfac16d33ca7d99bff30d953f0a1d9a40c
nicholas <nicholas.spong@adyen.com>
2025-01-24 13:28:05 +0100

    SessionProvider added and Login button connected to Google Auth

--- components/AuthProvider.jsx
+++ components/AuthProvider.jsx
@@ -1,4 +1,10 @@
 'use client';
+
+/**
+ * The SessionProvider has to be in a Client component, and ince the SessionProvider needs to be wrapped round all
+ * our content (in layout.jsx), and we don't want to make layout.jsx a Client component, we create this component
+ * which can then be added to layout.jsx
+ */
 import { SessionProvider } from 'next-auth/react';
 
 const AuthProvider = ({ children }) => {
