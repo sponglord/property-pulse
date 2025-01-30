b1617702b6b9fb4dbff8077f6e58aa3b03267e65
nicholas <nicholas.spong@adyen.com>
2025-01-30 17:22:27 +0100

    Added route that will retrieve properties based on user id

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -5,29 +5,30 @@ import Property from '@/models/Property';
 
 // Test by going to http://localhost:5040/api/properties/678e882ece5a98937f674389 (where the last string is an _id for an entry in the mongoDB)// GET api/properties/:[id]
 export const GET = async (request, { params }) => {
-    // N.B. second argument, destructuring params from the url
+	// N.B. second argument, destructuring params from the url
 
-    // Asynchronous access of `params.id`.
-    // In Next 15, the params & searchParam APIs have been made asynchronous. re. https://nextjs.org/docs/messages/sync-dynamic-apis
-    const { id } = await params;
+	try {
+		await connectDB();
 
-    try {
-        await connectDB();
+		// Asynchronous access of `params.id`.
+		// In Next 15, the params & searchParam APIs have been made asynchronous. re. https://nextjs.org/docs/messages/sync-dynamic-apis
+		const { id } = await params;
 
-        // The Property model has a findById method which allows you to specify the _id (db-added key) of a property you want to find
-        const property = await Property.findById(id);
+		// The Property model has a findById method which allows you to specify the _id (db-added key) of a property you want to find
+		const property = await Property.findById(id);
 
-        if (!property) return new Response('Property not found', { status: 404 });
+		if (!property)
+			return new Response('Property not found', { status: 404 });
 
-        /**
-         * What's happening...
-         *  - Property, being a model built with a Mongoose Schema, when it's "findById" method is called will
-         *    query the database we've connected to and retrieve the object with the matching _id
-         */
+		/**
+		 * What's happening...
+		 *  - Property, being a model built with a Mongoose Schema, when it's "findById" method is called will
+		 *    query the database we've connected to and retrieve the object with the matching _id
+		 */
 
-        return new Response(JSON.stringify(property), { status: 200 });
-    } catch (error) {
-        console.log('### GET error:: e=', error);
-        return new Response('Something went wrong', { status: 500 });
-    }
+		return new Response(JSON.stringify(property), { status: 200 });
+	} catch (error) {
+		console.log('### GET error:: e=', error);
+		return new Response('Something went wrong', { status: 500 });
+	}
 };
---
+++ app/api/properties/user/[userid]/route.js
@@ -0,0 +1,28 @@
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+
+// GET api/properties/user/:userid
+export const GET = async (request, { params }) => {
+	// For testing the route - go to http://localhost:5040/api/properties/user/[user id pulled from the DB]
+
+	// Note: We extract "userid" from params because that's what the folder containing this route is called
+	// If we'd just named the folder "id" then we'd be extracting "id" from params
+	const { userid } = await params;
+
+	if (!userid) {
+		return new Response('User ID is required', { status: 400 });
+	}
+
+	try {
+		await connectDB();
+
+		// The Property model has a find method which takes options on how you want to retrieve your data...
+		// ...here we just want the properties that match that user's id
+		const properties = await Property.find({ owner: userid });
+
+		return new Response(JSON.stringify(properties), { status: 200 });
+	} catch (error) {
+		console.log('### GET error:: e=', error);
+		return new Response('Something went wrong', { status: 500 });
+	}
+};
