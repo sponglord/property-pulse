57d7134184efde52801ac3748582cf36e1f055b2
nicholas <nicholas.spong@adyen.com>
2025-01-31 16:19:48 +0100

    Added Edit property page

--- middleware.js
+++ middleware.js
@@ -5,8 +5,15 @@ export const config = {
 	 * Match any urls we want to protect.
 	 * Without this, logging in (which then gives you access to the 'Add Property" page) & then logging out
 	 * - you would still be able to access http://localhost:5040/properties/add
+	 * - and http://localhost:5040/properties/679cde4fdbedf83845ffca76/edit
 	 *
 	 * With this, attempting to access any of these urls will lead you to a Google "sign in" button
 	 */
-	matcher: ['/properties/add', '/profile', '/properties/saved', '/messages'],
+	matcher: [
+		'/properties/add',
+		'/profile',
+		'/properties/saved',
+		'/messages',
+		'/properties/:id/edit', // If you log out whilst in Edit page you should be made to sign in
+	],
 };
