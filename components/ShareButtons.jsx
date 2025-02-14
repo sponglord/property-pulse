9b6fd468dc53f25114a147824810757e68360c0d
nicholas <nicholas.spong@adyen.com>
2025-02-14 17:13:33 +0100

    Added react-share & create share buttons

--- components/ShareButtons.jsx
+++ components/ShareButtons.jsx
@@ -1,10 +1,52 @@
-import { FaShare } from 'react-icons/fa';
+import {
+	FacebookShareButton,
+	TwitterShareButton,
+	WhatsappShareButton,
+	EmailShareButton,
+	FacebookIcon,
+	TwitterIcon,
+	WhatsappIcon,
+	EmailIcon,
+} from 'react-share';
 
-const ShareButtons = () => {
+const ShareButtons = ({ property }) => {
+	const shareURL = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`;
 	return (
-		<button className='bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
-			<FaShare className='mr-2' /> Share Property
-		</button>
+		<>
+			<h3 className='text-xl font-bold text-center pt-2'>
+				Share this Property:
+			</h3>
+			<div className='flex gap-3 justify-center pb-5'>
+				<FacebookShareButton
+					url={shareURL}
+					quote={property.name}
+					hashtag={`${property.type.replace(/\s/g, '')}ForRent`} // Remove spaces
+				>
+					<FacebookIcon size={40} round={true} />
+				</FacebookShareButton>
+				<TwitterShareButton
+					url={shareURL}
+					title={property.name}
+					hashtags={[`${property.type.replace(/\s/g, '')}ForRent`]} // Literally gives you a hashtag e.g. #LuxuryCondoForRent
+				>
+					<TwitterIcon size={40} round={true} />
+				</TwitterShareButton>
+				<WhatsappShareButton
+					url={shareURL}
+					title={property.name}
+					separator=':: '
+				>
+					<WhatsappIcon size={40} round={true} />
+				</WhatsappShareButton>
+				<EmailShareButton
+					url={shareURL}
+					subject={property.name}
+					body={`Check out this property listing: ${shareURL}`}
+				>
+					<EmailIcon size={40} round={true} />
+				</EmailShareButton>
+			</div>
+		</>
 	);
 };
 export default ShareButtons;
