6e048aafc8bd7246d060be3bc3688732726fa20c
nicholas <nicholas.spong@adyen.com>
2025-02-21 15:55:36 +0100

    Search comp added to Properties and Search results pages

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,30 +1,39 @@
 import PropertyCard from '@/components/PropertyCard';
 import { fetchProperties } from '@/utils/requests';
+import PropertySearchForm from '@/components/PropertySearchForm';
 
 const PropertiesPage = async () => {
-    // This is a server component - so we are "allowed" to call this function which will make a db call
-    const properties = await fetchProperties();
+	// This is a server component - so we are "allowed" to call this function which will make a db call
+	const properties = await fetchProperties();
 
-    // Sort properties by date
-    properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+	// Sort properties by date
+	properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
-    return (
-        <section className="px-4 py-6">
-            <div className="container-xl lg:container m-auto px-4 py-6">
-                {properties.length === 0 ? (
-                    <p>No properties found</p>
-                ) : (
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        {properties.map((property) => (
-                            <PropertyCard
-                                key={property._id}
-                                property={property}
-                            />
-                        ))}
-                    </div>
-                )}
-            </div>
-        </section>
-    );
+	return (
+		<>
+			<section className='bg-blue-700 py-4'>
+				{/* Note; the sm:px-6 lg:px-8 below relate to sizings from small (sm) and large (lg) screens */}
+				<div className='max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8'>
+					<PropertySearchForm />
+				</div>
+			</section>
+			<section className='px-4 py-6'>
+				<div className='container-xl lg:container m-auto px-4 py-6'>
+					{properties.length === 0 ? (
+						<p>No properties found</p>
+					) : (
+						<div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+							{properties.map((property) => (
+								<PropertyCard
+									key={property._id}
+									property={property}
+								/>
+							))}
+						</div>
+					)}
+				</div>
+			</section>
+		</>
+	);
 };
 export default PropertiesPage;
