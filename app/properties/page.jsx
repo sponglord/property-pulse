980f2146b195f20d5a2828c8e4bafd665d19fc93
nicholas <nicholas.spong@adyen.com>
2025-06-20 17:12:36 +0200

    Properties now retrieved from a clinet-side comp

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,14 +1,7 @@
-import PropertyCard from '@/components/PropertyCard';
-import { fetchProperties } from '@/utils/requests';
 import PropertySearchForm from '@/components/PropertySearchForm';
+import Properties from '@/components/Properties';
 
 const PropertiesPage = async () => {
-	// This is a server component - so we are "allowed" to call this function which will make a db call
-	const properties = await fetchProperties();
-
-	// Sort properties by date
-	properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
 	return (
 		<>
 			<section className='bg-blue-700 py-4'>
@@ -17,22 +10,8 @@ const PropertiesPage = async () => {
 					<PropertySearchForm />
 				</div>
 			</section>
-			<section className='px-4 py-6'>
-				<div className='container-xl lg:container m-auto px-4 py-6'>
-					{properties.length === 0 ? (
-						<p>No properties found</p>
-					) : (
-						<div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-							{properties.map((property) => (
-								<PropertyCard
-									key={property._id}
-									property={property}
-								/>
-							))}
-						</div>
-					)}
-				</div>
-			</section>
+
+			<Properties />
 		</>
 	);
 };
