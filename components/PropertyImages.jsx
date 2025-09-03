b5960e997160b1a8b93ce21b6f8c53ac78021631
nicholas <nicholas.spong@adyen.com>
2025-09-03 16:55:50 +0200

    Added a lightbox through which to view a property's uploaded images

--- components/PropertyImages.jsx
+++ components/PropertyImages.jsx
@@ -1,5 +1,6 @@
 import { getImageSrc } from '@/utils/getImageSrc';
 import Image from 'next/image';
+import { Gallery, Item } from 'react-photoswipe-gallery';
 
 /**
  * A dynamic grid for displaying images in 2 columns, but if there is an uneven number of images
@@ -10,29 +11,41 @@ const PropertyImages = ({ images }) => {
 	const firstImageSrc = getImageSrc(images[0]);
 
 	return (
-		<section className='bg-blue-50 p-4'>
-			<div className='container mx-auto'>
-				{images.length === 1 ? (
-					<Image
-						src={firstImageSrc}
-						alt=''
-						className='object-cover h-[400px] mx-auto rounded-xl'
-						width={1800}
-						height={400}
-						priority={true}
-					/>
-				) : (
-					// Create a grid with 2 columns when there are 2 or more images
-					<div className='grid grid-cols-2 gap-4'>
-						{images.map((image, index) => {
-							// Use util to display both images from Cloudinary, and, local ones
-							const imageSrc = getImageSrc(image);
+		<Gallery>
+			<section className='bg-blue-50 p-4'>
+				<div className='container mx-auto'>
+					{images.length === 1 ? (
+						<Item
+							original={firstImageSrc}
+							thumbnail={firstImageSrc}
+							width='1000'
+							height='600'
+						>
+							{({ ref, open }) => (
+								<Image
+									ref={ref}
+									onClick={open}
+									src={firstImageSrc}
+									alt=''
+									className='object-cover h-[400px] mx-auto rounded-xl'
+									width={1800}
+									height={400}
+									priority={true}
+								/>
+							)}
+						</Item>
+					) : (
+						// Create a grid with 2 columns when there are 2 or more images
+						<div className='grid grid-cols-2 gap-4'>
+							{images.map((image, index) => {
+								// Use util to display both images from Cloudinary, and, local ones
+								const imageSrc = getImageSrc(image);
 
-							return (
-								<div
-									key={index}
-									// Dynamic class - if there are an uneven number of images we want the last one to spread across both columns
-									className={`
+								return (
+									<div
+										key={index}
+										// Dynamic class - if there are an uneven number of images we want the last one to spread across both columns
+										className={`
                                         ${
 											images.length % 2 === 1 &&
 											index == images.length - 1
@@ -40,29 +53,41 @@ const PropertyImages = ({ images }) => {
 												: 'col-span-1'
 										}
                                         `}
-								>
-									<Image
-										src={imageSrc}
-										alt=''
-										className='object-cover h-[400px] w-full rounded-xl'
-										width={0}
-										height={0}
-										sizes='100vw'
-										/**
-										 * Avoids the:
-										 * "Image with src "https://blahblah.jpg" was detected as the Largest Contentful Paint (LCP).
-										 * Please add the "priority" property if this image is above the fold."
-										 * warning in the console
-										 */
-										priority={true}
-									/>
-								</div>
-							);
-						})}
-					</div>
-				)}
-			</div>
-		</section>
+									>
+										<Item
+											original={imageSrc}
+											thumbnail={imageSrc}
+											width='1000'
+											height='600'
+										>
+											{({ ref, open }) => (
+												<Image
+													ref={ref}
+													onClick={open}
+													src={imageSrc}
+													alt=''
+													className='object-cover h-[400px] w-full rounded-xl'
+													width={0}
+													height={0}
+													sizes='100vw'
+													/**
+													 * Avoids the:
+													 * "Image with src "https://blahblah.jpg" was detected as the Largest Contentful Paint (LCP).
+													 * Please add the "priority" property if this image is above the fold."
+													 * warning in the console
+													 */
+													priority={true}
+												/>
+											)}
+										</Item>
+									</div>
+								);
+							})}
+						</div>
+					)}
+				</div>
+			</section>
+		</Gallery>
 	);
 };
 export default PropertyImages;
